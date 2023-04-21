e7d0e5aee4240970d701482db2f6fa6aa65e2d6d
Gianmarco-Leone <gianmarcoleone96@gmail.com>
2023-04-21 17:21:25 +0200

    add favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,8 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    <link rel="shortcut icon" href="{{ asset('img/favicon.ico') }}" type="image/x-icon">
+
     <title>{{ config('app.name', 'Laravel') }} | @yield('page-name')</title>
 
 
