c207c806b2d8c8349ff633d1051ceff1ddf366c9
Gianmarco-Leone <gianmarcoleone96@gmail.com>
2023-04-28 19:01:11 +0200

    fixes validation

--- app/Http/Controllers/Api/MessageController.php
+++ app/Http/Controllers/Api/MessageController.php
@@ -19,28 +19,25 @@ class MessageController extends Controller
     public function store(Request $request)
     {
         // Validazione
-        // $request->validate([
-        //     '
-        //     $message->save();' => 'required|string|max:255',
-        //     '
-        //     $message->save();' => 'required|string|max:255|email:rfc,dns',
-        //     '
-        //     $message->save();' => 'required|text|max:65535',
-        // ],
-        // [
-        //    'author.required' => "Il nome dell'autore è obbligatorio", 
-        //    'author.string' => "Il nome dell'autore deve essere una stringa",
-        //    'author.max' => "Il nome dell'autore può contenere massimo 255 caratteri",
+        $request->validate([
+            'author' => 'required|string|max:255',
+            'email' => 'required|string|max:255|email:rfc,dns',
+            'text' => 'required|string|max:65535',
+        ],
+        [
+           'author.required' => "Il nome dell'autore è obbligatorio", 
+           'author.string' => "Il nome dell'autore deve essere una stringa",
+           'author.max' => "Il nome dell'autore può contenere massimo 255 caratteri",
 
-        //    'email.required' => "L'indirizzo email è obbligatorio", 
-        //    'email.string' => "L'indirizzo email deve essere una stringa",
-        //    'email.max' => "L'indirizzo email può contenere massimo 255 caratteri",
-        //    'email.email' => "L'indirizzo email deve per forza essere un indirizzo email valido",
+           'email.required' => "L'indirizzo email è obbligatorio", 
+           'email.string' => "L'indirizzo email deve essere una stringa",
+           'email.max' => "L'indirizzo email può contenere massimo 255 caratteri",
+           'email.email' => "L'indirizzo email deve per forza essere un indirizzo email valido",
 
-        //    'text.required' => "Il testo del messaggio è obbligatorio", 
-        //    'text.text' => "Il testo del messaggio deve essere una stringa",
-        //    'text.max' => "Il testo del messaggio può contenere massimo 65535 caratteri",
-        // ]);
+           'text.required' => "Il testo del messaggio è obbligatorio", 
+           'text.string' => "Il testo del messaggio deve essere una stringa",
+           'text.max' => "Il testo del messaggio può contenere massimo 65535 caratteri",
+        ]);
 
         $message = new Message();
         $message->fill($request->all());
